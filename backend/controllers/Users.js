658678230865c1ec8b0a54f519260a0751260199
Annisa Nurul Hakim <kakak.nisahakim@gmail.com>
2024-05-20 23:08:59 +0700

    push kedua

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -14,7 +14,6 @@ export const getUsers = async(req, res) => {
 }
 
 export const Register = async(req, res) => {
-    console.log(res);
     const { name, email, password, confPassword } = req.body;
     if (password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password Tidak Cocok"});
     const salt = await bcrypt.genSalt();
