c891287187d8fecdec96a0cbee13f6aad4ede6c8
nikenkhalilah <nikenhamuti@gmail.com>
2024-06-03 01:34:43 +0700

    profil

--- app.js
+++ app.js
@@ -3,39 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/Database');
+const profileRouter = require('./routes/profile');
+const app = express();
+const port = process.env.PORT || 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-var app = express();
+// Sync Database
+sequelize.sync().then(() => {
+    console.log('Database synced');
+}).catch((err) => {
+    console.error('Unable to sync database:', err);
+});
 
-// view engine setup
+// Set up EJS as template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/profile', profileRouter);
 
-app.use('/', usersRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,13 @@
 // File: routes/profile.js
 const express = require('express');
 const router = express.Router();
+const { getUsers, Register, Login, Logout, lihatProfil } = require("../controllers/Users.js");
 
 // Rute untuk menampilkan profil pengguna
 router.get('/', function(req, res) {
-    // Handler rute untuk menampilkan profil pengguna
+    
     res.render('profile', { user: req.user });
 });
+router.get('/:userId', lihatProfil);
 
 module.exports = router;
